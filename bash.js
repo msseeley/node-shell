23910e41a864cd5fe0c9e7982fedd191c02b707d
angel-chen <rikugou@gmail.com>
2018-04-24 12:41:12 -0400

    Complete pwd, ls commands

---
+++ bash.js
@@ -0,0 +1,22 @@
+const pwd = require('./pwd');
+const ls = require('./ls')
+
+process.stdout.write('prompt >');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim(); // remove the newline
+
+
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write('\nprompt > ');
+
+  if (cmd === 'pwd') {
+    pwd()
+  }
+
+  if (cmd === 'ls') {
+    ls()
+  }
+
+
+});
---
+++ pwd.js
@@ -0,0 +1,5 @@
+module.exports = function pwd () {
+
+  return process.stdout.write(__dirname)
+
+}
